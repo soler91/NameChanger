868cd87dddc6cf08dc3be46bda154d88b1fea1e2
Fruit <boskozarich@gmail.com>
2017-06-21 03:49:15 -0400

    Names are stored per character now.

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ module.exports = function NameChanger(dispatch) {
 	let newName = null,
 		enabled = true,
 		originalName = null;
+		character = [];
+		pId=null;
 	
 	try {
-		newName = require('./name.json')
+		character = require('./name.json')
 	}
 	catch(e) {}
 
@@ -20,8 +22,7 @@ module.exports = function NameChanger(dispatch) {
 		}
 		else if(str){
 		command.message('Name: '+str);
-		newName = str;
-		saveName();
+		AddAlias(str);
 		}
 	});
 	
@@ -37,6 +38,8 @@ module.exports = function NameChanger(dispatch) {
 	});
 	
 	dispatch.hook('S_LOGIN', 2, event => {
+		AddCharacter(event.playerId.toString(), event.name);
+		pId = event.playerId.toString();
 		if(enabled && newName) {
 			originalName = event.name;
 			event.name = newName;
@@ -46,17 +49,21 @@ module.exports = function NameChanger(dispatch) {
 	});
 	
 	dispatch.hook('S_CHAT', 1, event => {
-		if(event.authorName == originalName) {
-			event.authorName = newName;
-			return true;
+		if(enabled && newName) {
+			if(event.authorName == originalName) {
+				event.authorName = newName;
+				return true;
+			}
 		}
 		return
 	});
 	
 	dispatch.hook('S_WHISPER', 1, event => {
-		if(event.author == originalName) {
-			event.author = newName;
-			return true;
+		if(enabled && newName) {
+			if(event.author == originalName) {
+				event.author = newName;
+				return true;
+			}
 		}
 	});
 	
@@ -68,7 +75,36 @@ module.exports = function NameChanger(dispatch) {
 		return
 	});
 	
+	function AddCharacter(playerId, name){
+	let match = false;
+		for(let i in character){
+			if(character[i].playerId == playerId){
+				newName = character[i].alias;
+				match = true;
+			}
+		}
+		if(!match){
+			character.push({
+				playerId : playerId,
+				name : name,
+				alias : ''
+				});
+				newName = '';
+			saveName();
+		}
+	}
+	
+	function AddAlias(alias){
+		for(let l in character){
+			if(character[l].playerId == pId){
+				character[l].alias = alias;
+				newName = character[l].alias;
+			}
+		}
+		saveName();
+	}
+	
 	function saveName() {
-		fs.writeFileSync(path.join(__dirname, 'name.json'), JSON.stringify(newName))
+		fs.writeFileSync(path.join(__dirname, 'name.json'), JSON.stringify(character))
 	}
 }
